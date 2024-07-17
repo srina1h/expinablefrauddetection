7788eeec5800f4b2c32994addf7dacc0b09ce75f
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-07-17 08:37:51 -0400

    adding gitignore

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import torch
 import openai
 import streamlit as st
 
-def option_choice():
-    genre = st.radio(
-        "Do you want to select a sample text file OR upload your own?",
-        ('Select Sample text file', 'Upload my own')
-    )
-    if genre == "Select Sample text file": flag=False
-    else: flag = True
-    return flag
-
 if __name__ == "__main__":
     st.set_page_config(
         page_title="Explainable Fraud Detection",
